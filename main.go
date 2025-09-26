f0767b48967f017bd2cf280fea0ec3201eb68d52
imranhasan871 <imranhasan871@gmail.com>
2025-09-26 18:24:44 +0600

    fix: refactor product handling and initialize product list

--- main.go
+++ main.go
@@ -1,32 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(w, "Hello World")
+type Product struct {
+	ID          int
+	Title       string
+	Description string
+	Price       float64
+	ImgURL      string
 }
 
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(w, "I'm Imran. I am Best software engineer.")
-}
+var ProductList []Product
 
 func getProducts(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, "Please give me GET request", http.StatusBadRequest)
+		return
 	}
 
-	fmt.Println(w, "This is your all products")
+	w.Header().Set("content-type", "application/json")
+
+	encoder := json.NewEncoder(w)
+	error := encoder.Encode(ProductList)
+	if error != nil {
+
+	}
 }
 
 func main() {
 	mux := http.NewServeMux()
-
-	mux.HandleFunc("/hello", helloHandler)
-	mux.HandleFunc("/about", aboutHandler)
 
 	mux.HandleFunc("/products", getProducts)
 
@@ -36,4 +43,55 @@ func main() {
 		fmt.Println("Error starting the server")
 		os.Exit(1)
 	}
+}
+
+func init() {
+	product1 := Product{
+		ID:          1,
+		Title:       "Wireless Headphones",
+		Description: "High-quality noise-cancelling headphones.",
+		Price:       129.99,
+		ImgURL:      "https://www.lovefoodhatewaste.com/sites/default/files/styles/twitter_card_image/public/2022-07/Citrus%20fruits.jpg.webp?itok=H1j9CCCS",
+	}
+	product2 := Product{
+		ID:          2,
+		Title:       "Smart Watch",
+		Description: "Stylish smart watch with health tracking.",
+		Price:       199.99,
+		ImgURL:      "https://i0.wp.com/post.healthline.com/wp-content/uploads/2021/05/apples-1296x728-header.jpg?w=1155&h=1528",
+	}
+	product3 := Product{
+		ID:          3,
+		Title:       "Running Shoes",
+		Description: "Lightweight shoes for everyday running.",
+		Price:       89.50,
+		ImgURL:      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZbMOVB8a8wRQ6e-UKZggiu7-edRAN1GolPQ&s",
+	}
+	product4 := Product{
+		ID:          4,
+		Title:       "Wireless Headphones",
+		Description: "High-quality noise-cancelling headphones.",
+		Price:       129.99,
+		ImgURL:      "https://www.lovefoodhatewaste.com/sites/default/files/styles/twitter_card_image/public/2022-07/Citrus%20fruits.jpg.webp?itok=H1j9CCCS",
+	}
+	product5 := Product{
+		ID: 5, Title: "Smart Watch",
+		Description: "Stylish smart watch with health tracking.",
+		Price:       199.99,
+		ImgURL:      "https://i0.wp.com/post.healthline.com/wp-content/uploads/2021/05/apples-1296x728-header.jpg?w=1155&h=1528",
+	}
+	product6 := Product{
+		ID:          6,
+		Title:       "Running Shoes",
+		Description: "Lightweight shoes for everyday running.",
+		Price:       89.50,
+		ImgURL:      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZbMOVB8a8wRQ6e-UKZggiu7-edRAN1GolPQ&s",
+	}
+
+	ProductList = append(ProductList, product1)
+	ProductList = append(ProductList, product2)
+	ProductList = append(ProductList, product3)
+	ProductList = append(ProductList, product4)
+	ProductList = append(ProductList, product5)
+	ProductList = append(ProductList, product6)
 }
