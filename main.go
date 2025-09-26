b8f44591feef83f7e9362dacfd8ea129a451566c
imranhasan871 <imranhasan871@gmail.com>
2025-09-26 17:36:19 +0600

    Add initial implementation of the HTTP server with hello and about handlers

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Hello World")
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "I'm Imran. I am Best software engineer.")
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/hello", helloHandler)
+	mux.HandleFunc("/about", aboutHandler)
+
+	fmt.Println("Server is running on port 3000")
+	err := http.ListenAndServe(":3000", mux)
+	if err != nil {
+		fmt.Println("Error starting the server")
+		os.Exit(1)
+	}
+}
