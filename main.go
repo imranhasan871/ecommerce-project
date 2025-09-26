5e0fb1e4796d529fcd86eeff0ffd1eb958552b99
imranhasan871 <imranhasan871@gmail.com>
2025-09-26 17:57:23 +0600

    feat: add getProducts handler and update .gitignore for executable files

--- main.go
+++ main.go
@@ -14,11 +14,21 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(w, "I'm Imran. I am Best software engineer.")
 }
 
+func getProducts(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Please give me GET request", http.StatusBadRequest)
+	}
+
+	fmt.Println(w, "This is your all products")
+}
+
 func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/hello", helloHandler)
 	mux.HandleFunc("/about", aboutHandler)
+
+	mux.HandleFunc("/products", getProducts)
 
 	fmt.Println("Server is running on port 3000")
 	err := http.ListenAndServe(":3000", mux)
